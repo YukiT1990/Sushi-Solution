66657fb1f71e752d19fb0f659ed34dd0a336f523
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-04-01 18:16:25 +0900

    main.swift -> solution

---
+++ SushiSolution/main.swift
@@ -0,0 +1,174 @@
+//
+//  main.swift
+//  SushiSolution
+//
+//  Created by Yuki Tsukada on 2021/04/01.
+//
+
+import Foundation
+
+public final class Queue<E> : Sequence {
+    /// beginning of queue
+    private var first: Node<E>? = nil
+    /// end of queue
+    private var last: Node<E>? = nil
+    /// size of the queue
+    private(set) var count: Int = 0
+    
+    /// helper linked list node class
+    fileprivate class Node<E> {
+        fileprivate var item: E
+        fileprivate var next: Node<E>?
+        fileprivate init(item: E, next: Node<E>? = nil) {
+            self.item = item
+            self.next = next
+        }
+    }
+    
+    /// Initializes an empty queue.
+    public init() {}
+    
+    /// Returns true if this queue is empty.
+    public func isEmpty() -> Bool {
+        return first == nil
+    }
+    
+    /// Returns the item least recently added to this queue.
+    public func peek() -> E? {
+        return first?.item
+    }
+    
+    /// Adds the item to this queue
+    /// - Parameter item: the item to add
+    public func enqueue(item: E) {
+        let oldLast = last
+        last = Node<E>(item: item)
+        if isEmpty() { first = last }
+        else { oldLast?.next = last }
+        count += 1
+    }
+    
+    /// Removes and returns the item on this queue that was least recently added.
+    public func dequeue() -> E? {
+        if let item = first?.item {
+            first = first?.next
+            count -= 1
+            // to avoid loitering
+            if isEmpty() { last = nil }
+            return item
+        }
+        return nil
+    }
+    
+    /// QueueIterator that iterates over the items in FIFO order.
+    public struct QueueIterator<E> : IteratorProtocol {
+        private var current: Node<E>?
+        
+        fileprivate init(_ first: Node<E>?) {
+            self.current = first
+        }
+        
+        public mutating func next() -> E? {
+            if let item = current?.item {
+                current = current?.next
+                return item
+            }
+            return nil
+        }
+        
+        public typealias Element = E
+    }
+    
+    /// Returns an iterator that iterates over the items in this Queue in FIFO order.
+    public __consuming func makeIterator() -> QueueIterator<E> {
+        return QueueIterator<E>(first)
+    }
+}
+
+extension Queue: CustomStringConvertible {
+    public var description: String {
+        return self.reduce(into: "") { $0 += "\($1) " }
+    }
+}
+
+
+func solution() {
+    struct Restaurant {
+        let number: Int
+        let adjacent: [Int]
+        let previousRestaurant: Int
+        let traveledRestaurants: Set<Int>
+        let travelTime: Int
+    }
+    
+    let firstLine = readLine()!.split(separator: " ").map { Int($0) }
+    let numOfN = firstLine[0]!
+    let numOfM = firstLine[1]!
+    let indexesOfM: [Int] = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    var smallestTravelTime = 2 * numOfN
+    
+    var paths = [[Int]](repeating: [Int](repeating: 0, count: 0), count: numOfN)
+    
+    for _ in 1..<numOfN {
+        let pathInput = readLine()!.split(separator: " ").map { Int($0) }
+        paths[pathInput[0]!].append(pathInput[1]!)
+        paths[pathInput[1]!].append(pathInput[0]!)
+    }
+    
+    func findSmallestTravelTime(startRestaurant: Restaurant) {
+        let q = Queue<Restaurant>()
+        q.enqueue(item: startRestaurant)
+        var restaurantsToVisitDict: [Int: Bool] = [:]
+        for i in indexesOfM {
+            restaurantsToVisitDict[i] = false
+        }
+        
+        while !q.isEmpty() {
+            
+            let sq = q.dequeue()!
+            let number = sq.number
+            let adjacentRestaurants: [Int] = sq.adjacent
+            let previousReataurant: Int = sq.previousRestaurant
+            var traveledRestaurants: Set<Int> = sq.traveledRestaurants
+            let travelTime = sq.travelTime
+            traveledRestaurants.insert(number)
+            
+            if travelTime > smallestTravelTime {
+                break
+            }
+            if indexesOfM.contains(number) {
+                restaurantsToVisitDict[number] = true
+                if containsAll(array: indexesOfM, set: traveledRestaurants) {
+                    if travelTime < smallestTravelTime {
+                        smallestTravelTime = travelTime
+                    }
+                    break
+                }
+            }
+            for i in 0..<adjacentRestaurants.count {
+                let adjacentRestaurantNumber = adjacentRestaurants[i]
+                if adjacentRestaurantNumber == previousReataurant && adjacentRestaurants.count > 1 {
+                    continue
+                }
+                q.enqueue(item: Restaurant(number: adjacentRestaurantNumber, adjacent: paths[adjacentRestaurantNumber], previousRestaurant: number, traveledRestaurants: traveledRestaurants, travelTime: travelTime + 1))
+            }
+        }
+    }
+    for restaurant in indexesOfM {
+        let emptySet: Set<Int> = []
+        findSmallestTravelTime(startRestaurant: Restaurant(number: restaurant, adjacent: paths[restaurant], previousRestaurant: -1, traveledRestaurants: emptySet, travelTime: 0))
+    }
+    print(smallestTravelTime)
+}
+
+func containsAll(array: [Int], set: Set<Int>) -> Bool {
+    for i in array {
+        if !set.contains(i) {
+            return false
+        }
+    }
+    return true
+}
+
+solution()
